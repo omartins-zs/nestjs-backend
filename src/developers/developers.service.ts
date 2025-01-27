f40b92155b85993b226b42b11b2d9e70bd112ef3
omartins-zs <gabrielmatheus209@gmail.com>
2025-01-27 10:33:34 -0300

    Ajustando os Metodos no Service

--- src/developers/developers.service.ts
+++ src/developers/developers.service.ts
@@ -1,26 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { CreateDeveloperDto } from './dto/create-developer.dto';
 import { UpdateDeveloperDto } from './dto/update-developer.dto';
+import { Developer } from './entities/developer.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class DevelopersService {
-  create(createDeveloperDto: CreateDeveloperDto) {
-    return 'This action adds a new developer';
+  constructor(
+    private readonly repository: Repository<Developer>) {}
+  create(dto: CreateDeveloperDto) {
+    const developer = this.repository.create(dto);
+    return this.repository.save(developer);
+    // return 'This action adds a new developer';
   }
 
   findAll() {
-    return `This action returns all developers`;
+    return this.repository.find();
+
+    // return `This action returns all developers`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} developer`;
+  findOne(id: string) {
+    return this.repository.findOneBy({ id });
+
+    // return `This action returns a #${id} developer`;
   }
 
-  update(id: number, updateDeveloperDto: UpdateDeveloperDto) {
-    return `This action updates a #${id} developer`;
+  async update(id: string, dto: UpdateDeveloperDto) {
+    const developer = await this.repository.findOneBy({ id });
+    if (!developer) return null;
+    this.repository.merge(developer, dto);
+    return this.repository.save(developer);
+
+    // return `This action updates a #${id} developer`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} developer`;
+  async remove(id: string) {
+    const developer = await this.repository.findOneBy({ id });
+    if (!developer) return null;
+    return this.repository.remove(developer);
+    // return `This action removes a #${id} developer`;
   }
 }
